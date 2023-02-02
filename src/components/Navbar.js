498b9d6886e3c8d87821aea66df2727ea222a390
DHEERAJ2AUS <108796274+DHEERAJ2AUS@users.noreply.github.com>
2023-02-02 12:04:39 +0530

    JIRJTIWREJ

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,7 +16,7 @@ const Navbar = () => {
         
     </a>
     <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-      <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+      {/* <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
         
         <li>
          { user ?
@@ -32,7 +32,7 @@ const Navbar = () => {
           >Login</button>
          }
         </li>
-      </ul>
+      </ul> */}
     </div>
   </div>
 </nav>
